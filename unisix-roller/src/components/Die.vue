268f20b6f6f8f96a357b2eb732c4bd2da791ee40
Aly <alex.mitan@yahoo.com>
2024-10-12 23:18:44 +0300

    initial

---
+++ unisix-roller/src/components/Die.vue
@@ -0,0 +1,32 @@
+<script setup lang="js">
+import { ref } from "vue";
+
+const { maxValue, currentValue } = defineProps({
+  maxValue: String,
+  currentValue: Number,
+});
+let _maxValue = Number(maxValue);
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+</script>
+<template>
+  <div class="die">
+    <button type="button" @click="">
+      value is {{ getRandomInt(1, _maxValue) }} /
+      {{ maxValue }}
+    </button>
+  </div>
+</template>
+
+<style scoped>
+.die {
+  display: inline-block;
+  float: left;
+  width: 100px;
+  height: 50px;
+}
+</style>
